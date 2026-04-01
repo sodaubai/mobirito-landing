db8b0f8ed2ef81432d73cba0a48429284374187c
Shadow Engineer <huynq.code@gmail.com>
2026-04-01 10:01:26 +0200

    fix: make header fixed with blur background for all pages

--- components/StoryblokComponents.js
+++ components/StoryblokComponents.js
@@ -184,7 +184,7 @@ function BlogPostCard({ blok }) {
 
 function SiteNavbar({ blok }) {
   return (
-    <header style={{position:"absolute",width:"100%",zIndex:10}}>
+    <header style={{position:"fixed",width:"100%",zIndex:50,top:0}}>
       <div className="container">
         <div className="navbar">
           <a className="logo" href="/">
